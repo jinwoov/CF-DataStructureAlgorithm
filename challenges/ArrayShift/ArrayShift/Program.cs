51353638368ee49096e0583787331d970565065c
Jin Kim <jinwoov@gmail.com>
2020-03-17 22:40:04 -0700

    finished stretch goal

--- challenges/ArrayShift/ArrayShift/Program.cs
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -8,12 +8,9 @@ namespace ArrayShift
         static void Main(string[] args)
         {
 
-            int[] testArray = new[] { 4, 8, 15, 23, 4 };
+            int[] testArray = new[] { 4, 8, 15, 23, 42 };
             int[] resultArray = InsertShiftArray(testArray, 16);
-           foreach (int value in resultArray)
-           {
-               Console.WriteLine(value);
-           }
+            Console.WriteLine("This is the output after inserting a value to middle {0}", String.Join(", ", resultArray));
         }
 
         public static int[] InsertShiftArray(int[] array, int numberArg)
@@ -24,14 +21,45 @@ namespace ArrayShift
             // Declaring variable that is half of argument array length and converting it to int data type
             decimal halfOfDec = array.Length / 2m;
             halfOfDec = Math.Ceiling(halfOfDec);
+            // Converting the decimal type to int type
             int halfOfArrayLength = Convert.ToInt32(halfOfDec);
             
             // for loop to iterate and instantiate into newArray
             for (int i = 0; i < newArray.Length; i++)
             {
+                // Only happens when index is half of array length
                 if (i == halfOfArrayLength)
                 {
+                    // Inserting new value in
                     newArray[i] = numberArg;
+                    // decrementing to stay in same spot for old array element instead of skipping over
+                    ind--;
+                }
+                else
+                {
+                    newArray[i] = array[ind];
+                }
+                // Counter for old array index
+                ind++;
+            }
+
+            return newArray;
+        }
+
+        public static int[] StretchGoal(int[] array)
+        {
+            // Declare a variables
+            int[] newArray = new int[array.Length-1];
+            int ind = 0;
+            // create a half point mark
+            int halfOfArrayLength = array.Length/2;
+
+            // run a for loop
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (i >= halfOfArrayLength)
+                {
+                    newArray[i] = array[++ind];
                     ind--;
                 }
                 else
@@ -41,13 +69,8 @@ namespace ArrayShift
 
                 ind++;
             }
-
+            // return the new array
             return newArray;
-        }
-
-        public static int[] StretchGoal(int[] array)
-        {
-
         }
     }
 }
--- challenges/ArrayShift/shitArrayTesting/UnitTest1.cs
+++ challenges/ArrayShift/shitArrayTesting/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace shitArrayTesting
 {
     public class UnitTest1
     {
+        // testing inserting a value to middle
         [Fact]
         public void Test1()
         {
@@ -20,6 +21,22 @@ namespace shitArrayTesting
             int[] testArray = new[] { 4, 8, 15, 23, 42 };
             int[] resultArray = new[] { 4, 8, 15, 16, 23, 42 };
             Assert.Equal(resultArray, Program.InsertShiftArray(testArray, 16));
+        }
+
+        //testing deleting middle elements
+        [Fact]
+        public void DeletingMiddleElement()
+        {
+            int[] testArray = new[] { 4, 8, 15, 23, 42 };
+            int[] resultArray = new[] { 4, 8, 23, 42 };
+            Assert.Equal(resultArray, Program.StretchGoal(testArray));
+        }
+        [Fact]
+        public void DeletingMiddleElement1()
+        {
+            int[] testArray = new[] { 2, 4, 6, 8 };
+            int[] resultArray = new[] { 2, 4, 8 };
+            Assert.Equal(resultArray, Program.StretchGoal(testArray));
         }
     }
 }
