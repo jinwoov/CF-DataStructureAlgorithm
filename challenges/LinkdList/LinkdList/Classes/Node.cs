b990a4dc22f36f357f47396842b87061a4fa25e7
Jin Kim <jinwoov@gmail.com>
2020-03-23 04:01:29 -0700

    mostly finished with the logic and created recursive methods for printing

---
+++ challenges/LinkdList/LinkdList/Classes/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkdList.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// data for the node
+        /// </summary>
+        public int Data { get; set; }
+
+        /// <summary>
+        /// next value for node
+        /// </summary>
+        public Node Next { get; set; }
+
+        public void PrintNode()
+        {
+            Console.Write($"{Data} ->");
+            if (Next != null)
+            {
+                Next.PrintNode();
+            }
+            else
+            {
+            Console.Write("NULL");
+            }
+        }
+    }
+}
---
+++ challenges/LinkdList/LinkdList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LinkdList.Classes;
+
+namespace LinkdList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linklist ll = new Linklist();
+
+            ll.Insert(2);
+            ll.Insert(3);
+            ll.Insert(4);
+            ll.Insert(5);
+
+            bool search = ll.Includes(3);
+            Console.WriteLine(search.ToString()); 
+
+            ll.ToString();
+        }
+    }
+}
