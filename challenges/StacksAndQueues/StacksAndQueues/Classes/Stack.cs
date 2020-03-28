2142d84f55322d747e342bd9b39a2afdf37c33e5
Jin Kim <jinwoov@gmail.com>
2020-03-28 15:12:41 -0700

    push method is created

--- challenges/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
+++ challenges/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -6,6 +6,15 @@ namespace StacksAndQueues.Classes
 {
     class Stack
     {
+        public Node Top { get; set; } = null;
 
+        public void Push(int value)
+        {
+            Node OldTop = Top;
+            Top = new Node(value);
+            Top.Next = OldTop;
+        }
+
+        public 
     }
 }
