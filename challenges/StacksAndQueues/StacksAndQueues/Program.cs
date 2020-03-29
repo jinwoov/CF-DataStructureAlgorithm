16edab09510c5628425c8ffcb14465e5cad49cb0
Jin Kim <jinwoov@gmail.com>
2020-03-28 19:58:48 -0700

    implementing

--- challenges/StacksAndQueues/StacksAndQueues/Program.cs
+++ challenges/StacksAndQueues/StacksAndQueues/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StacksAndQueues.Classes;
+using System;
 
 namespace StacksAndQueues
 {
@@ -7,8 +8,20 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            QueueImplementation();
         }
 
+        static void QueueImplementation()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+
+            int resultOfDequeue = queue.Dequeue();
+            Console.WriteLine($"This is result from queue from dequeuing, expected output is 1: {resultOfDequeue}");
+
+            
+        }
 
     }
 }
