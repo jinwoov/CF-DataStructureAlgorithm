27f48bcb52cc88b92782e58478d6599d1084a1fd
Jin Kim <jinwoov@gmail.com>
2020-03-30 18:24:27 -0700

    added all of the summary in the class

--- challenges/QueueWithStacks/QueueWithStacks/Classes/PseudoQueue.cs
+++ challenges/QueueWithStacks/QueueWithStacks/Classes/PseudoQueue.cs
@@ -9,12 +9,19 @@ namespace QueueWithStacks.Classes
         public Stack PseudoQ { get; set; }
         public Stack PseudoQTwo { get; set; }
 
+        /// <summary>
+        /// Constructor instantiate an object when PseudoQueue object is instantiated
+        /// </summary>
         public PseudoQueue()
         {
             this.PseudoQ = new Stack();
             this.PseudoQTwo = new Stack();
         }
 
+        /// <summary>
+        /// Moving off all of the first stack object node to the second object then add it, then added back from second object
+        /// </summary>
+        /// <param name="value">Value that will be added to the new queue</param>
         public void Enqueue(int value)
         {
                 while (PseudoQ.Top != null)
@@ -30,13 +37,16 @@ namespace QueueWithStacks.Classes
                 }
         }
 
+        /// <summary>
+        /// This is to check if the object is empty first which will raise exception when it is, other wise it will pop the queue and return its value
+        /// </summary>
+        /// <returns>Popped value</returns>
         public int Dequeue()
         {
             if (PseudoQ == null) 
             {
                 throw new IndexOutOfRangeException("Your queue is empty");
             }
-
 
             return PseudoQ.Pop();
 
