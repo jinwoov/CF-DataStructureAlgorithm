e33880b3a5fc6a7fd53c3170e39421bd81269db7
Jin Kim <jinwoov@gmail.com>
2020-04-01 14:13:57 -0700

    Summary is done

--- challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Animal.cs
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Animal.cs
@@ -10,6 +10,11 @@ namespace FIFOAnimalShelter.Classes
         public string Type { get; set; }
         public int LineNumber { get; set; }
 
+        /// <summary>
+        ///  Construtor that will assign name and type when object is instantiated using arguments that been passed in
+        /// </summary>
+        /// <param name="name">name of the animal</param>
+        /// <param name="type">type of the animal</param>
         public Animal(string name, string type)
         {
             this.Name = name;
