de214e79fa932363ee889f7b2d2afb592c3ce1c7
Jin Kim <jinwoov@gmail.com>
2020-04-06 22:51:11 -0700

    Added last test

--- challenges/FizzBuzzTree/GUnitFiftyCents/UnitTest1.cs
+++ challenges/FizzBuzzTree/GUnitFiftyCents/UnitTest1.cs
@@ -14,8 +14,30 @@ namespace GUnitFiftyCents
             BinarySearchTree bts = Program.FizzBuzzTree(bst);
 
             Assert.Equal("Buzz", bts.Root.StringValue);
+        }
 
+        [Fact]
+        public void TestingToSeeIfAddedThreeIsShowingFizz()
+        {
+            BinarySearchTree bst = new BinarySearchTree(5);
+            bst.Add(bst.Root, 3);
+            BinarySearchTree bts = Program.FizzBuzzTree(bst);
 
+            string result = bts.Root.Left.StringValue;
+
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void ExpectedFail()
+        {
+            BinarySearchTree bst = new BinarySearchTree(5);
+            bst.Add(bst.Root, 5);
+            BinarySearchTree bts = Program.FizzBuzzTree(bst);
+
+            string result = bts.Root.Right.StringValue;
+
+            Assert.NotEqual("FizzBuzz", result);
         }
     }
 }
