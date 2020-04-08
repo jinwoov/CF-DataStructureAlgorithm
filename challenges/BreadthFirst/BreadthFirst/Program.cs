27287d745e7642ff44082c81f346c77a4fe22c4f
Jin Kim <jinwoov@gmail.com>
2020-04-07 19:21:42 -0700

    methods complete

---
+++ challenges/BreadthFirst/BreadthFirst/Program.cs
@@ -0,0 +1,33 @@
+﻿using BreadthFirst.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace BreadthFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestingMethod();
+        }
+
+        static void TestingMethod()
+        {
+            BinaryTree bt = new BinaryTree(5);
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Add(bt.Root, 1);
+            bst.Add(bt.Root, 2);
+            bst.Add(bt.Root, 6);
+            bst.Add(bt.Root, 7);
+
+            List<int> list = BinaryTree.BreathFirst(bt);
+
+            foreach (var item in list)
+            {
+                Console.Write($"{item} => ");
+            }
+            Console.Write("END");
+
+        }
+    }
+}
