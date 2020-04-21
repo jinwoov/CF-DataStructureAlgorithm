07aaa793a228312d4154e2223c26548a1ab538d1
Jin Kim <jinwoov@gmail.com>
2020-04-20 20:35:29 -0700

    implemented the insertionSort

---
+++ challenges/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 8, 15, 42, 16, 23 };
+
+            int [] newArray = InsertionSort(array);
+
+            foreach (var number in newArray)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
+
+                while(j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j -= 1;
+                }
+                arr[j + 1] = temp;
+            }
+            return arr;
+        }
+    }
+}
